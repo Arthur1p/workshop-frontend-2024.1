37ebb8e54d286f66664d16e6d3145bf790609770
unknown <arthurformigademelo2@Gmail.comgit config --global user.name Arthurgit config --global user.name Arthurgit config --global user.email arthurformigademelo2@gmail.com>
2024-03-09 11:30:03 -0300

    v1.1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Header.css'
 
+const handleInicioClick = () => {
+    window.location.href="index.html"
+}
+
 function Header(){
     return(
         <header>
@@ -9,7 +13,7 @@ function Header(){
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0nY4ATllrd5qOML2WXtWYr5-f396xNrxGY8Fj9U1wvQ&s"
                      alt="logo" className='logo' />
                 </div>
-                <button className='inicioButton'>Inicio</button>
+                <button className='inicioButton' onClick={handleInicioClick}>Inicio</button>
             </nav>
         </header>
     )
